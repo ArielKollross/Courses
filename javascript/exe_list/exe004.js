91ab7ecf09209b9c222335be4cd215f852cfa228
Ariel Kollross <arielkollross@gmail.com>
2020-02-11 23:58:12 -0300

    exe upload

---
+++ javascript/exe_list/exe004.js
@@ -0,0 +1,13 @@
+/*
+Crie uma função que irá receber dois valores, o dividendo e o divisor. A função deverá imprimir o resultado
+e o resto da divisão destes dois valores.
+*/
+
+const divisor = (a,b) =>{
+    div = a/b
+    div2 = a%2
+    console.log(div,div2)
+}
+
+divisor(4,2)
+
